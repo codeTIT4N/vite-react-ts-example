68e108600e10375454d0d23aa185f0507012e7e3
codeTIT4N <lokeshkumar636@yahoo.com>
2023-02-05 04:26:43 +0530

    fin

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import "./App.css"
+import { useMultistepForm } from './useMultistepForm'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { steps, currentStepIndex, step, isFirstStep, isLastStep, back, next } = useMultistepForm([
+    <h1 >Just trying out Vite + React + TS</h1>,
+    <h1 >Looks pretty similar to CRA so far!</h1>,
+    <h1 >Very fast project setup</h1>,
+    <h1 >The HMR is amazing!</h1>,
+    <h1 >Thankyou!</h1>
+  ])
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div style={{
+      position: "relative",
+      background: "white",
+      border: "1px solid black",
+      padding: "2rem",
+      margin: "1rem",
+      borderRadius: ".5rem",
+      fontFamily: "Ariel",
+      marginTop: "20%"
+    }}>
+      <form>
+        <div style={{ position: "absolute", top: ".5rem", right: ".5rem" }}>
+          {currentStepIndex + 1} / {steps.length}
+        </div>
+        {step}
+        <div style={{
+          marginTop: "1rem",
+          display: "flex",
+          gap: ".5rem",
+          justifyContent: "flex-end"
+        }}></div>
+        {!isFirstStep && <button type='button' onClick={back}>Back</button>}
+        <button type='button' onClick={next}>{isLastStep ? "Finish" : "Next"}</button>
+      </form>
     </div>
   )
 }
